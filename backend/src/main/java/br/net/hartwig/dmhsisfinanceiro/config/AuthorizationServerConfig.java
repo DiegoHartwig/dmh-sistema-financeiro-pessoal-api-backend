5547933c82e394885c89e7210a20397cc74ee90e
Diego Hartwig <hartwig.diego@gmail.com>
2020-04-03 22:58:49 -0300

    Implementando autenticacao com OAuth2 e Token JWT

--- backend/src/main/java/br/net/hartwig/dmhsisfinanceiro/config/AuthorizationServerConfig.java
+++ backend/src/main/java/br/net/hartwig/dmhsisfinanceiro/config/AuthorizationServerConfig.java
@@ -9,39 +9,38 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-	
+
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory()
-			.withClient("angular")
-			.secret("@angular0")
-			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(1800);
-	}
-	
-	@Override
-	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-			.tokenStore(tokenStore())
-			.authenticationManager(authenticationManager);
-	}
-	
-	@Bean
-	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		clients.inMemory().withClient("angular").secret("@angular0").scopes("read", "write")
+				.authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
 	}
 
-	
-	
-	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter())
+				.authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("hartwig");
+		return accessTokenConverter;
+	}
+
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
 
 }
